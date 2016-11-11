bbaab2789d0c31abe4be16cd5d4eb0a979727d98
健銘孫 <makodoben@gmail.com>
2016-11-11 15:04:08 +0800

    Add the init seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Order.create(name: “Bob”, phone: “0920000000”, description: “Big Mac”)
+Order.create(name: “Mary”, phone: “0910000000”, description: “Little Mac”)
+Order.create(name: “Andy”, phone: “0930000000”, description: “Meduim Mac”)
